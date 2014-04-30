c733edd0ae590eae76169a9d05a2614f850722ee
Manuel Selva <manuel.selva@insa-lyon.fr>
2014-04-30 17:17:16 +0200

    added numa allocation and number of iterations support to the mem_load program

--- mem_load/mem_load.c
+++ mem_load/mem_load.c
@@ -10,6 +10,9 @@
 #include <sys/time.h> // For gettimeofday
 #include <string.h>
 #include <cpuid.h>
+#include <numa.h>
+//#include <numaif.h>
+
 
 #define ONE     p = (uint64_t *)*p;
 #define FIVE    ONE ONE ONE ONE ONE
@@ -18,43 +21,67 @@
 #define	HUNDRED	FIFTY FIFTY
 
 void usage(const char *prog_name) {
-  printf ("Usage %s: size_in_kb mode (where mode is either seq or rand)\n", prog_name);
+  printf ("Usage: %s size mode node nb_iter\n"
+	  "  size is the size of allocated and accessed memory in kb\n"
+	  "  mode is either seq or rand for sequential or random accesses\n"
+	  "  node is the NUMA node where memory must be explicitely allocated (-1 for standard malloc)\n"
+	  "  nb_iter is the number of time the iteration reading over one hundred elements is done (-1 == infinite loop)\n", prog_name);
 }
 
 int main(int argc, char **argv) {
 
- /**
-  * Check and get arguments.
-  */
-  if (argc != 3) {
+  /**
+   * Check and get arguments.
+   */
+  if (argc != 5) {
     usage(argv[0]);
     return -1;
   }
-  size_t size = atol(argv[1]) * 1024;
+  size_t size_in_bytes = atol(argv[1]) * 1024;
   enum access_mode_t access_mode;
   if (!strcmp(argv[2], "seq")) {
     access_mode = access_seq;
   } else if (!strcmp(argv[2], "rand")) {
     access_mode = access_rand;
   } else {
-    printf("Unknown access_mode %s\n", argv[3]);
+    printf("Unknown access_mode %s\n", argv[2]);
     usage(argv[0]);
     return -1;
   }
+  char node = atoi(argv[3]);
+  register int nb_iter = atoi(argv[4]);
 
   /**
    * Allocate and fill memory
    */
-  uint64_t *memory = malloc(size);
+  uint64_t *memory;
+  if (node == -1) {
+    memory = malloc(size_in_bytes);
+  } else {
+    memory = numa_alloc_onnode(size_in_bytes, node);
+  }
   assert(memory);
-  fill_memory(memory, size, access_mode);
+  fill_memory(memory, size_in_bytes, access_mode);
   register uint64_t *p = memory;
 
-  /**
-   * Infinite loop
-   */
-  while (1) {
-    HUNDRED
+  if (nb_iter == -1) {
+    /**
+     * Infinite loop
+     */
+    while (1) {
+      HUNDRED
+    }
+  } else {
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+    int register i = 0;
+    while (i < nb_iter) {
+      i++;
+      HUNDRED;
+    }
+    clock_gettime(CLOCK_REALTIME, &end);
+    uint64_t ns = (end.tv_sec * 1E9 + end.tv_nsec) - (start.tv_sec * 1E9 + start.tv_nsec);
+    fprintf(stderr, "time = %" PRIu64 " ns\n", ns);
   }
 
   return 0;
